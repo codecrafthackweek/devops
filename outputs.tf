2312b00171e4e367b91113c8201babd789528c04
codecrafthackweek <148720546+codecrafthackweek@users.noreply.github.com>
2023-10-30 20:33:42 -0300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "instance_ip" {
+  value = aws_instance.codecraft_vm.public_ip
+  description = "O IP público da instância EC2"
+}
+output "instance_id" {
+  value = aws_instance.codecraft_vm.id
+}
+output "instance_public_dns" {
+  value = aws_instance.codecraft_vm.public_dns
+}

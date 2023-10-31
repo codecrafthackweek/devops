d9942071484fe1b4bb44eabac94461f1424e3927
codecrafthackweek <148720546+codecrafthackweek@users.noreply.github.com>
2023-10-31 08:22:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,14 @@ resource "aws_security_group" "codecraft_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
